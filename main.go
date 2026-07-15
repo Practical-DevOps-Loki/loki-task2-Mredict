0f444cebe48fb62750d2d89a5c1f1cc1d6acb048
Mredict <144549206+Mredict@users.noreply.github.com>
2026-07-15 14:33:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,35 +1,18 @@
 package main
 
 import (
-	"io"
-	"os"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+    "github.com/labstack/echo/v4"
 	"webapp/pkg/config"
 )
 
 func main() {
 	port := config.GetEnv("PORT", "3000")
-	logPath := config.GetEnv("LOG_PATH", "/app/log/app.log")
 
-	e := echo.New()
-	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	
-	if err == nil {
-		multiWriter := io.MultiWriter(os.Stdout, file)
-		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-			Output: multiWriter,
-		}))
-	} else {
-		e.Use(middleware.Logger())
-	}
-
-	e.Static("/", "public")
-
-	e.GET("/", func(c echo.Context) error {
-		return c.File("public/views/webapp.html")
-	})
-
-	e.Logger.Fatal(e.Start(":" + port))
+    e := echo.New()
+    e.Static("/", "public")
+    
+    e.GET("/", func(c echo.Context) error {
+        return c.File("public/views/webapp.html")
+    })
+    e.Start(":" + port)
 }
