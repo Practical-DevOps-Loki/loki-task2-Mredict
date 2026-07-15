45b9fa948f4446b61cdd2f2266c8333f1fdbde57
Mredict <144549206+Mredict@users.noreply.github.com>
2026-07-15 14:26:35 +0300

    Add file logging to Echo server
    Implement logging to a file with fallback to stdout.

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-    "github.com/labstack/echo/v4"
+	"io"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"webapp/pkg/config"
 )
 
 func main() {
 	port := config.GetEnv("PORT", "3000")
+	logPath := config.GetEnv("LOG_PATH", "/app/log/app.log")
 
-    e := echo.New()
-    e.Static("/", "public")
-    
-    e.GET("/", func(c echo.Context) error {
-        return c.File("public/views/webapp.html")
-    })
-    e.Start(":" + port)
+	e := echo.New()
+	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	
+	if err == nil {
+		multiWriter := io.MultiWriter(os.Stdout, file)
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Output: multiWriter,
+		}))
+	} else {
+		e.Use(middleware.Logger())
+	}
+
+	e.Static("/", "public")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.File("public/views/webapp.html")
+	})
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
